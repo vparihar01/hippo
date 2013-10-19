fc74d6de7081425800fe67ae6fb6f1a28b128742
Amitkumar Jha <amit@weboniselab.com>
2013-10-19 14:50:35 +0530

    added live validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  before_filter :authorize!
   private
 
   def current_user
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user
 
-  def authorize
-    redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  def authorize!
+    redirect_to root_path, alert: "Not authorized" unless current_user.present?
   end
 end
