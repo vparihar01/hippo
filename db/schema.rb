ae4d3d7b44f20bbe7c700c9e16feb946905a51b9
Vivek Parihar <vivek@weboniselab.com>
2013-10-19 18:12:01 +0530

    1. cloud provider instaces done 2. insatnces listing done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131019084715) do
+ActiveRecord::Schema.define(:version => 20131019113940) do
 
   create_table "cloud_providers", :force => true do |t|
     t.string   "key"
@@ -21,6 +21,19 @@ ActiveRecord::Schema.define(:version => 20131019084715) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "type"
+  end
+
+  create_table "instances", :force => true do |t|
+    t.string   "title"
+    t.string   "instance_id"
+    t.string   "state"
+    t.string   "flavor_id"
+    t.string   "name"
+    t.string   "private_ip"
+    t.string   "public_ip"
+    t.string   "cloud_provider_id"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
   end
 
   create_table "users", :force => true do |t|
