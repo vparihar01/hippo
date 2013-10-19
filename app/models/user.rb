198476a95abe21802a07f84815c168262caebc83
Amitkumar Jha <amit@weboniselab.com>
2013-10-19 12:35:14 +0530

    added guest user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :username, :email, :password, :password_confirmation
-
-  validates_presence_of :email, :password_digest, unless: :guest?
-#  validates_uniqueness_of :username, allow_blank: true
- # validates_confirmation_of :password
-
-  # override has_secure_password to customize validation until Rails 4.
-  require 'bcrypt'
-  attr_reader :password
-  include ActiveModel::SecurePassword::InstanceMethodsOnActivation
+  attr_accessible :email
+  #validates_uniqueness_of :email
 
   def self.new_guest
     new { |u| u.guest = true }
   end
 
   def name
-    "Guest"
+    guest ? "Guest" : email
   end
 
   def move_to(user)
