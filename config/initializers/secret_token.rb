9ff941f71cba117b0aabd410baab96a931b08ae3
Amitkumar Jha <amit@weboniselab.com>
2013-10-19 10:48:09 +0530

    Initializing Hippo.. A centralized Access for cloud services

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hippo::Application.config.secret_token = '00f71c920180cc48a6d839d4c58c4afed9dada903882dc3ee02b8e6b374eadf597d3d3d286434a9ea3d1c2f9beefd7656d76e33555dd165f3a8ea4ca1e8fd764'
