f1cef3b4a10609948c4a6529858086e3a103f8c6
Amitkumar Jha <amit@weboniselab.com>
2013-10-19 11:48:40 +0530

    added bootstrap layout

--- Gemfile
+++ Gemfile
@@ -6,8 +6,10 @@ gem 'rails', '3.2.13'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'mysql2'
-
-
+gem "therubyracer"
+gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
+gem "twitter-bootstrap-rails"
+gem 'bcrypt-ruby', '~> 3.0.0'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Hippo::Application.routes.draw do
+  resources :users
+
+
+  get "homes/index"
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+resources :users
+resources :sessions
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +58,7 @@ Hippo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'homes#index'
 
   # See how all your routes lay out with "rake routes"
 
