1e4e87c5d042f9ebdbf7b77e3bd73a3d8248a581
Akash Badiyani <akash@weboniselab.com>
2013-10-20 11:43:29 +0530

    dashboard completed

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".iconsWrapper a").mouseenter(function(){
+        $(this).stop().animate({
+            marginTop : -10
+        }, 100, function() {
+            // Animation complete.
+        });
+    });
+    $(".iconsWrapper a").mouseleave(function(){
+        $(this).stop().animate({
+            marginTop : 0
+        }, 100, function() {
+            // Animation complete.
+        });
+    });
+});
